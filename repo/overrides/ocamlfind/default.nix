b373f33c3b3bd18082286c7c95b914583b60fb98
Tim Cuthbertson <tim@gfxmonk.net>
2016-08-06 17:42:16 +1000

    include $dep/lib in LD_LIBRARY_PATH

--- repo/overrides/ocamlfind/default.nix
+++ repo/overrides/ocamlfind/default.nix
@@ -26,6 +26,10 @@ in {
 		addOCamlPath () {
 			if test -d "''$1/lib"; then
 				export OCAMLPATH="''${OCAMLPATH}''${OCAMLPATH:+:}''$1/lib"
+
+				# used only for .byte targets, but doesn't hurt in general:
+				export LD_LIBRARY_PATH="''${LD_LIBRARY_PATH}''${LD_LIBRARY_PATH:+:}''$1/lib"
+
 				if test -d "''$1/lib/stublibs"; then
 					export CAML_LD_LIBRARY_PATH="''${CAML_LD_LIBRARY_PATH}''${CAML_LD_LIBRARY_PATH:+:}''$1/lib/stublibs"
 				fi
