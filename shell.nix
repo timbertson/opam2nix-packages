dca497dac9a5224bc76e039b9c48ea2188083954
Tim Cuthbertson <tim@gfxmonk.net>
2017-02-11 16:37:43 +1100

    include git in shell.nix

--- shell.nix
+++ shell.nix
@@ -3,5 +3,5 @@ let
 	base = import ./nix/local.nix { inherit pkgs; };
 in
 pkgs.lib.overrideDerivation base (o: {
-	nativeBuildInputs = o.nativeBuildInputs ++ [pkgs.gup];
+	nativeBuildInputs = o.nativeBuildInputs ++ [pkgs.gup pkgs.git];
 })
