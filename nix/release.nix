141c9b6ab707d6119b4e5b887371119d912451ef
travis-ci <travis-ci@gfxmonk.net>
2017-03-26 09:35:05 +0000

    Bump release.nix

--- nix/release.nix
+++ nix/release.nix
@@ -8,14 +8,14 @@ let
 	src = fetchgit 	{
 		"url" = "https://github.com/timbertson/opam2nix-packages.git";
 		"fetchSubmodules" = false;
-		"sha256" = "12ziqcan40rrsq66mvkw09vswi8liy6ds5q526g0smb1x3s5dyhl";
-		"rev" = "8b4704f16fdab6d760e1fbebcc7be710a590493b";
+		"sha256" = "130px0sp9lq3isl02da3n0250gv5a0lbcqmyx36qn3d7y5xsijjr";
+		"rev" = "8ca05d152980384a5b426e64b419eee2b9d9ba89";
 	};
 	opam2nix = fetchgit 	{
 		"url" = "https://github.com/timbertson/opam2nix.git";
 		"fetchSubmodules" = false;
-		"sha256" = "1sr0fnw8m071g3h6phr21jm6568gs9y3ll3izypb3hvacgc6x3vr";
-		"rev" = "dca65798560f75ee76a89e75255d81107609314d";
+		"sha256" = "0vf0k0y817fc8x9h1rg26ngvz3wyvwx600qh9p98dk3mai88cyga";
+		"rev" = "96c8d83a3262917562b243ebb5d0a8da0c7335a3";
 	};
 in
 if devRepo != "" then
