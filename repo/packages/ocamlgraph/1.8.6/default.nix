676a5d6be7f3b01deb88f31df317d5ef0c38d8c8
Tim Cuthbertson <tim@gfxmonk.net>
2017-10-15 12:16:36 +1100

    ocamlgraph

---
+++ repo/packages/ocamlgraph/1.8.6/default.nix
@@ -0,0 +1,44 @@
+world:
+let
+    fetchurl = pkgs.fetchurl;
+    inputs = lib.filter (dep: dep != true && dep != null)
+    ([  ] ++ (lib.attrValues opamDeps));
+    lib = pkgs.lib;
+    opam2nix = world.opam2nix;
+    opamDeps = 
+    {
+      conf-gnomecanvas = opamSelection.conf-gnomecanvas or null;
+      lablgtk = opamSelection.lablgtk or null;
+      ocaml = opamSelection.ocaml;
+      ocamlfind = opamSelection.ocamlfind;
+    };
+    opamSelection = world.opamSelection;
+    pkgs = world.pkgs;
+in
+pkgs.stdenv.mkDerivation 
+{
+  buildInputs = inputs;
+  buildPhase = "${opam2nix}/bin/opam2nix invoke build";
+  configurePhase = "true";
+  installPhase = "${opam2nix}/bin/opam2nix invoke install";
+  name = "ocamlgraph-1.8.6";
+  opamEnv = builtins.toJSON 
+  {
+    deps = opamDeps;
+    files = null;
+    name = "ocamlgraph";
+    ocaml-version = world.ocamlVersion;
+    spec = ./opam;
+  };
+  passthru = 
+  {
+    opamSelection = opamSelection;
+  };
+  propagatedBuildInputs = inputs;
+  src = fetchurl 
+  {
+    sha256 = "1vyvy9cazv1grh0xn0j47w8bnhw93jbn29cqv34k1r0px16yyxdx";
+    url = "http://ocamlgraph.lri.fr/download/ocamlgraph-1.8.6.tar.gz";
+  };
+}
+
