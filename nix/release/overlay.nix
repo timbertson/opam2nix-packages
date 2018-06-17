fcd8e948ac18708d4c04468af6fcd056fb9830b1
Tim Cuthbertson <tim@gfxmonk.net>
2018-06-17 16:18:32 +1000

    release

--- nix/release/overlay.nix
+++ nix/release/overlay.nix
@@ -3,17 +3,17 @@ self: super: with super; {
 		src = fetchgit {
 			"url" = "https://github.com/timbertson/opam2nix-packages.git";
 			"fetchSubmodules" = true;
-			"sha256" = "0ai6p6rlryy1akasrn3jm7kqlv9x4d9sllg8byxkq8yv9aw068ag";
-			"rev" = "13325c55b1f6a56bbb76dac9009aa25a6f54b4b9";
+			"sha256" = "1xf8wy5zcaj81v7nr3wwrhr6qjz22k8c2grwwbb74q0py5yhxwj6";
+			"rev" = "40db0c95aeaa63b60ccdb85cb63118fce27038af";
 		};
-		opam2nixSrc = fetchgit {
-			"url" = "https://github.com/timbertson/opam2nix.git";
-			"fetchSubmodules" = true;
-			"sha256" = "0fanhwa94n7l0y962k7pivffdrv5838hqq8byy279zzhzzszww0d";
-			"rev" = "79caf71f87d9b75b20a1f02ab91dec0c2f4e7555";
+		opam2nixSrc = fetchFromGitHub {
+			"owner" = "timbertson";
+			"repo" = "opam2nix";
+			"sha256" = "1khq1b0c7ry8854nwl0qkfq0kddf4g49xmj1yp2bifk8kh2waqb7";
+			"rev" = "version-0.3.1";
 		};
-	in
-	callPackage "${src}/nix" {
 		opam2nixBin = callPackage "${opam2nixSrc}/nix" {};
-	};
+	in
+	callPackage "${src}/nix" { inherit opam2nixBin; }
+;
 }
