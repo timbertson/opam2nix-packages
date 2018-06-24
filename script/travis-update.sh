9697a53f9a88a46bd9b3a7728e26c110e95ef64f
Tim Cuthbertson <tim@gfxmonk.net>
2018-06-24 20:56:37 +1000

    fix travis-update.sh

--- script/travis-update.sh
+++ script/travis-update.sh
@@ -4,15 +4,14 @@ set -eux
 git config user.name 'travis-ci';
 git config user.email 'travis-ci@gfxmonk.net';
 
-mkdir -p opam-repository
-pushd opam-repository
+if [ ! -e opam-repository ]; then
 	git clone git://github.com/ocaml/opam-repository.git
-popd
+fi
 
 rm -f .push
-gup repo/packages
+gup repo/packages nix/release/src-opam-repository.json
 if ! git diff --quiet --exit-code; then
-	git add -u repo/digest.json nix/
+	git add -u repo nix/
 	git commit -m "Automated package update";
 	gup nix/all
 	git commit -u -m "Bump release nix expressions" nix
