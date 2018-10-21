aa5d437eb57a66052d37b829ac02c93349cd6f7c
Tim Cuthbertson <tim@gfxmonk.net>
2018-10-21 13:41:02 +1100

    bump release

--- nix/release/default.nix
+++ nix/release/default.nix
@@ -4,14 +4,14 @@ let
 		src = fetchFromGitHub {
 			"owner" = "timbertson";
 			"repo" = "opam2nix-packages";
-			"sha256" = "1p220dfqhwfp3v5b9y2qf7fvi4n2wargsdq5japr0slyiwqdp5f0";
-			"rev" = "e83b3a41b8bd1f9cb37265ecdf1850928a7edcb1";
+			"sha256" = "0rajh0a986yvd4h0svwjb71ylyi4lj7b96bkqn85d1wmbs55gj2h";
+			"rev" = "93558942398ad17b1665e04cb8a8af18b8e64b9d";
 		};
 		opam2nixSrc = fetchFromGitHub {
 			"owner" = "timbertson";
 			"repo" = "opam2nix";
-			"sha256" = "0dlr0m25a6hi6yybg3my5vp1022gkvyjbyfpq28yqmd7iasg5wg5";
-			"rev" = "version-0.4.2";
+			"sha256" = "0jjbi7v93yw6s6187nwcfk93s1vs9haaxppihy7ggqaj2rvziknj";
+			"rev" = "version-0.4.3";
 		};
 		opam2nixBin = callPackage "${opam2nixSrc}/nix" {};
 	in
