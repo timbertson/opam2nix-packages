3c1ebab5a36f70f9d8762390cf21c7c6e06984ce
Tim Dysinger <tim@dysinger.net>
2018-11-01 13:30:49 -1000

    4_06 is the default ocaml now in nixpkgs/nixos

--- single-shell.sh
+++ single-shell.sh
@@ -2,4 +2,4 @@
 set -eux
 pkg="$1"
 shift
-exec nix-pin shell --no-out-link --path nix/default.nix --show-trace -A opamPackages.4_05."$pkg" "$@"
+exec nix-pin shell --no-out-link --path nix/default.nix --show-trace -A opamPackages.4_06."$pkg" "$@"
