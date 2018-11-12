6399f3adb76f0af631cf4f980d4e0c9642b980e0
Tim Cuthbertson <tim@gfxmonk.net>
2018-11-12 12:35:09 +1100

    fix release/default.nix

--- nix/release/default.nix
+++ nix/release/default.nix
@@ -2,6 +2,6 @@ let fetch = pkgs: srcJson: (pkgs.nix-update-source.fetch srcJson).src; in
 {
 	pkgs ? import <nixpkgs> {},
 	opam2nixBin ? pkgs.callPackage "${fetch pkgs ./src-opam2nix.json}/nix" {},
-	opamRepository ? fetch ./src-opam-repository.json,
+	opamRepository ? fetch pkgs ./src-opam-repository.json,
 }:
 pkgs.callPackage "${fetch pkgs ./src.json}/nix" { inherit opam2nixBin opamRepository; }
