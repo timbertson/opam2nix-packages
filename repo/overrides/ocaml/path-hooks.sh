5580ca19457f8b7e2d5e2798e36e5aea10ca9990
François Bobot <francois.bobot@cea.fr>
2019-02-18 22:13:59 +0100

    Add package directories to CAML_LD_LIBRARY_PATH
    Dynamic libraries are installed in package directories. So we add these directories to CAML_LD_LIBRARY_PATH.

--- repo/overrides/ocaml/path-hooks.sh
+++ repo/overrides/ocaml/path-hooks.sh
@@ -5,7 +5,7 @@ function ocamlPathSetup {
 		if test -d "$1/lib/stublibs"; then
 			export CAML_LD_LIBRARY_PATH="${CAML_LD_LIBRARY_PATH}${CAML_LD_LIBRARY_PATH:+:}$1/lib/stublibs"
 		else
-			export CAML_LD_LIBRARY_PATH="${CAML_LD_LIBRARY_PATH}${CAML_LD_LIBRARY_PATH:+:}$1/lib"
+			export CAML_LD_LIBRARY_PATH="${CAML_LD_LIBRARY_PATH}${CAML_LD_LIBRARY_PATH:+:}$(find $1/lib -maxdepth 1 -mindepth 1 -printf %p:)$1/lib"
 		fi
 	fi
 }
