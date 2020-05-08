d2819732f5110e2b75b856d0bd08a000e4a1fc7d
François Bobot <francois.bobot@cea.fr>
2020-05-08 16:44:02 +0200

    Protect variable reading in bash using :-

--- repo/overrides/ocaml/path-hooks.sh
+++ repo/overrides/ocaml/path-hooks.sh
@@ -1,11 +1,11 @@
 function ocamlPathSetup {
 	if test -d "$1/lib"; then
-		export OCAMLPATH="${OCAMLPATH}${OCAMLPATH:+:}$1/lib"
+		export OCAMLPATH="${OCAMLPATH:-}${OCAMLPATH:+:}$1/lib"
 
 		if test -d "$1/lib/stublibs"; then
-			export CAML_LD_LIBRARY_PATH="${CAML_LD_LIBRARY_PATH}${CAML_LD_LIBRARY_PATH:+:}$1/lib/stublibs"
+			export CAML_LD_LIBRARY_PATH="${CAML_LD_LIBRARY_PATH:-}${CAML_LD_LIBRARY_PATH:+:}$1/lib/stublibs"
 		else
-			export CAML_LD_LIBRARY_PATH="${CAML_LD_LIBRARY_PATH}${CAML_LD_LIBRARY_PATH:+:}$1/lib"
+			export CAML_LD_LIBRARY_PATH="${CAML_LD_LIBRARY_PATH:-}${CAML_LD_LIBRARY_PATH:+:}$1/lib"
 		fi
 	fi
 }
